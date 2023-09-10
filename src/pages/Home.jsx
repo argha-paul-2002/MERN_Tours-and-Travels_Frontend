6a19a29ddfda833db88c0509839ff53e64f37c28
Argha Paul <arghapaul002@gmail.com>
2023-09-10 12:42:49 +0530

    Hero section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React from 'react';
+import '../styles/home.css';
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import worldImg from '../assets/images/world.png'
+import Subtitle from '../shared/Subtitle';
+
+import SearchBar from '../shared/SearchBar';
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+    {/* ============== Hero Section Start ================ */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+            <div className="hero__content">
+              <div className="hero__subtitle d-flex align-items-center">
+                <Subtitle subtitle={'Know Before You Go'} />
+                <img src={worldImg} alt="" />
+              </div>
+              <h1>Traveling opens the door to creating <span className="highlight">memories</span></h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem excepturi asperiores nam commodi deleniti eos deserunt hic dicta laudantium alias, cumque tempora nulla culpa quisquam et ratione incidunt voluptatem voluptatibus.</p>
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className="hero__img-box">
+              <img src={heroImg} alt="" />
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className="hero__img-box mt-4 ">
+              <video src={heroVideo} alt="" controls/>
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className="hero__img-box mt-5 ">
+              <img src={heroImg02} alt="" />
+            </div>
+          </Col>
+          <SearchBar />
+        </Row>
+      </Container>
+    </section>
+    {/* ============== Hero Section End ================ */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='3'>
+            <h5 className="services__subtitle">What we serve</h5>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    </>
   )
 }
 
