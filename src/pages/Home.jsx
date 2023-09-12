0164c5b0dc350621819d82d323e176bbd43134e4
Argha Paul <arghapaul002@gmail.com>
2023-09-12 17:16:37 +0530

    Service List section design done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import worldImg from '../assets/images/world.png'
 import Subtitle from '../shared/Subtitle';
 
 import SearchBar from '../shared/SearchBar';
+import ServiceList from '../services/ServiceList';
 
 const Home = () => {
   return (
@@ -51,7 +52,9 @@ const Home = () => {
         <Row>
           <Col lg='3'>
             <h5 className="services__subtitle">What we serve</h5>
+            <h2 className='services__title'>We offer our best services</h2>
           </Col>
+          <ServiceList />
         </Row>
       </Container>
     </section>
